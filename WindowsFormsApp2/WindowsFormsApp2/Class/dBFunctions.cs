87c9706d1a1f152645e03355d9743187a21a1938
unknown <andrasgaal0209@gmail.com>
2024-07-12 10:58:58 +0200

    commit

---
+++ WindowsFormsApp2/WindowsFormsApp2/Class/dBFunctions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace WindowsFormsApp2.Class
+{
+    internal class dBFunctions
+    {
+        public static string ConnectionStringSQLite
+        { 
+        get
+            {
+
+                string database = AppDomain.CurrentDomain.BaseDirectory +
+                    "\\DataBase\\Contact_files\\Database\\Contact.s3db";
+                string connectionString = @"Data Source=" + database;
+                return connectionString;
+            }
+        }
+    }
+}
---
+++ WindowsFormsApp2/WindowsFormsApp2/Forms/ContactList.cs
@@ -0,0 +1,32 @@
+﻿using Kribo.Class;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Core.Common.CommandTrees;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WindowsFormsApp2.Class;
+
+namespace WindowsFormsApp2.Forms
+{
+    public partial class ContactList : Form
+    {
+        public ContactList()
+        {
+            InitializeComponent();
+        }
+
+        private void ContactList_Load(object sender, EventArgs e)
+        {
+            dBHelper db = new dBHelper(dBFunctions.ConnectionStringSQLite);
+            db.Load("SELECT*FROM Contact");
+            DataTable dataTabel = db.DataSet.Tables[0];
+            dataGridView1.DataSource = dataTabel;
+        }
+    }
+}
